8338db0e613bad34bb09d3a6d598b8042032c477
Robert O'Shea <robertosheagamedev@gmail.com>
2026-04-21 01:10:33 +0100

    Add SCSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,39 @@
-module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/css/*.css");
-    eleventyConfig.addPassthroughCopy("src/assets/*");
+import path from "node:path";
+import * as sass from "sass";
 
-    return {
-        dir: {
-            input: "src",
-            output: "_site"
-        }
-    };
-};
+export default function (eleventyConfig) {
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css",
+
+    useLayouts: false,
+
+    compile: async function (inputContent, inputPath) {
+      let parsed = path.parse(inputPath);
+      // Don’t compile file names that start with an underscore
+      if (parsed.name.startsWith("_")) {
+        return;
+      }
+
+      let result = sass.compileString(inputContent, {
+        loadPaths: [parsed.dir || ".", this.config.dir.includes],
+      });
+
+      // Map dependencies for incremental builds
+      this.addDependencies(inputPath, result.loadedUrls);
+
+      return async (data) => {
+        return result.css;
+      };
+    },
+  });
+  eleventyConfig.addTemplateFormats("scss");
+  eleventyConfig.addPassthroughCopy("src/css/*.css");
+  eleventyConfig.addPassthroughCopy("src/assets/*");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+    },
+  };
+}
