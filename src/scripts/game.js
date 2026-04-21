2a6ab332c745fedb33502b66ee91263694555ea5
Robert O'Shea <robertosheagamedev@gmail.com>
2026-04-21 18:21:44 +0100

    Move canvase removal

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -34,6 +34,19 @@ function startGame() {
 
 document.onreadystatechange = function () {
   if (document.readyState == "complete") {
-    startGame();
+    const isMobileUserAgent = () => {
+      return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent,
+      );
+    };
+
+    if (isMobileUserAgent()) {
+      let canvas = document.getElementById("game");
+      if (canvas !== null) {
+        canvas.outerHTML = "<p>Game is not playable on mobile! Sorry!<p>";
+      }
+    } else {
+      startGame();
+    }
   }
 };
